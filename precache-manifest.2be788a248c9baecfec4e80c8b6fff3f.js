f035a15575bae1f7b5c6fc38be5fdd9e51feeac0
Lefty93 <yongchuseng@gmail.com>
2020-11-03 23:03:22 +0800

    Updates

---
+++ precache-manifest.2be788a248c9baecfec4e80c8b6fff3f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cd586ac0960beda97ea7f75fa0de79d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e284bc62e790135e08ac",
+    "url": "/static/css/main.276e5d37.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e284bc62e790135e08ac",
+    "url": "/static/js/main.7460f2ce.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
